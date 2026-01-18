412cac622a880f7d33844dcfd546cfe657b33b9c
Kateryna Koliada <6parkseolhwa6@gmail.com>
2026-01-18 10:33:01 +0100

    Add model

---
+++ src/models/product.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const productsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      default: 'other',
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Product = mongoose.model('Product', productsSchema);
