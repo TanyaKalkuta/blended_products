9fede27ded0f059978474e2b3f5dbaf5ad441856
Tetiana Kalkuta <72383361+TanyaKalkuta@users.noreply.github.com>
2026-01-18 11:28:38 +0000

    add patch

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,10 +1,16 @@
-import { Router } from "express";
-import { getProducts, getProductById, createProduct } from "../controllers/productsController.js";
+import { Router } from 'express';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+} from '../controllers/productsController.js';
 
 const productsRouter = Router();
 
 productsRouter.get('/products', getProducts);
 productsRouter.get('/products/:productId', getProductById);
 productsRouter.post('/products', createProduct);
+productsRouter.patch('/products/:productId', updateProduct);
 
 export default productsRouter;
